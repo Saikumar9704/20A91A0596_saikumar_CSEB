afbebb3a4a4f74827778b442458e4793e156ead6
Saikumar9704 <84527949+Saikumar9704@users.noreply.github.com>
2021-05-20 22:25:04 +0530

    Create Identationerror.py

---
+++ Identationerror.py
@@ -0,0 +1,13 @@
+a=10
+b=20
+if a<b:
+	  print("a is small")
+	      print("a value is 10")
+	  print("hi··········")
+else:
+	print("a is big")
+	print("hi·······")
+	
+···
+expected output
+Indentattion Error: unecxpected
