d32bd46d4d4534ee4dfff8cfdc5373c3e6167323
Saikumar9704 <84527949+Saikumar9704@users.noreply.github.com>
2021-05-21 10:31:35 +0530

    Create Controlstatement(if).py

---
+++ Controlstatement(if).py
@@ -0,0 +1,12 @@
+a=int(input())
+#check whether a is <10 or not
+if a<10:
+	  print('a is less than 10')
+	  print('Hi..............')
+	  
+	  '''
+	  output
+	  8
+	  a is less than 10
+	  Hi.........
+	  '''
