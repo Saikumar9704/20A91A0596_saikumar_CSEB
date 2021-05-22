2b77e7a60f9ffa1132f6d375ecc25e1567d98a1f
Saikumar9704 <84527949+Saikumar9704@users.noreply.github.com>
2021-05-22 13:51:21 +0530

    Create Even or odd.py

---
+++ Even or odd.py
@@ -0,0 +1,16 @@
+num=int(raw_input("enter the number")
+if(num%2==0):
+    print("given number is even")
+else:
+    print("given number is odd")
+
+''' 
+expected output
+enter the number2
+given number is even
+'''
+'''
+Output
+enter the number2
+given number is even
+'''
