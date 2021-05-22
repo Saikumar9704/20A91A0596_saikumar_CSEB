58b48faae39d7eda1977c2544119abe079621a1c
Saikumar9704 <84527949+Saikumar9704@users.noreply.github.com>
2021-05-22 14:28:10 +0530

    Create (Elif).py

---
+++ (Elif).py
@@ -0,0 +1,23 @@
+num=int(raw_input())
+if(num%2==0):
+    print("divisible by 2")
+if(num%3==0):
+    print("divisible by 3")
+if(num%4==0):
+    print("divisible by 4")
+if(num%5==0):
+    print("divisible by 5")
+else:
+    print("number is not divisible by 2,3,4,5")
+ 
+'''
+Case 1:
+8
+divisible by 2
+Case 2:
+9
+divisible by 3
+Case 3:
+4
+divisible by 2
+'''
