8f019ec3963789194c7fcc674f2e843ee749b686
Saikumar9704 <84527949+Saikumar9704@users.noreply.github.com>
2021-05-22 12:45:31 +0530

    Create (if_else).py

---
+++ (if_else).py
@@ -0,0 +1,16 @@
+program to find age and check if they are eligible for vote or not
+if else
+name=input('enter name:')
+age=int(input('enter age:'))
+if age>=18
+print('you are eligible for vote',name)
+else
+print('you are not elgible for vote',name)
+
+
+
+
+output:
+enter name:sai kumar
+enter age:18
+you are eligible for vote sai kumar
